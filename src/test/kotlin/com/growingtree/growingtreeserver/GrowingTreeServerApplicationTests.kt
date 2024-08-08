7b50543a83f07271db3c6ad035138615a2d9c7d6
Hyobeen-Park <amanda0109@naver.com>
2024-08-09 01:30:40 +0900

    Initial Commit

---
+++ src/test/kotlin/com/growingtree/growingtreeserver/GrowingTreeServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.growingtree.growingtreeserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrowingTreeServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
