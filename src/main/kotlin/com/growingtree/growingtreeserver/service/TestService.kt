2c35a1feeadbd2c6e08ec70c6a9dfc109c5a795a
Hyobeen-Park <amanda0109@naver.com>
2024-08-11 05:41:11 +0900

    [ADD] #1 ktlint 추가 및 pr-checker 수정

--- src/main/kotlin/com/growingtree/growingtreeserver/service/TestService.kt
+++ src/main/kotlin/com/growingtree/growingtreeserver/service/TestService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 @Service
 @RequiredArgsConstructor
 class TestService {
-    fun getTest() : String {
+    fun getTest(): String {
         return "Hello World"
     }
 }
--- src/test/kotlin/com/growingtree/growingtreeserver/GrowingTreeServerApplicationTests.kt
+++ src/test/kotlin/com/growingtree/growingtreeserver/GrowingTreeServerApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class GrowingTreeServerApplicationTests {
-
     @Test
     fun contextLoads() {
     }
-
 }
