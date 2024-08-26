710bb8973ff980d8b1c247f4762bb4a2a44a0dad
Hyobeen-Park <amanda0109@naver.com>
2024-08-26 13:43:50 +0900

    [ADD/#4] plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
+    kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "com.growingtree"
